b3de40b63462692ecc97e70095728a75a809ded9
RafaelEtiene <105309826+RafaelEtiene@users.noreply.github.com>
2022-06-02 13:55:10 -0300

    js

--- Exercícios/exercício site/script.js
+++ Exercícios/exercício site/script.js
@@ -1,9 +1,23 @@
-var h = new Date()
-var agora = h.getHours()
-
-var txh = document.getElementById('txth')
-txh.innerHTML = `Agora são ${agora} horas.`
-
-if(h < 12) {
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var h = data.getHours()
+    h = 15
+    
+    msg.innerHTML = `Agora são ${h} horas.`
+    if(h >= 0 && h < 12)  {
+        //Bom dia
+        img.src = 'modelo/manhã.png'
+        document.body.style.background = '5555'
+    } else if(h >= 12 && h < 18) {
+        
+        img.src = 'modelo/tarde.png'
+        document.body.style.background = '#898466'
+    } else{
+        
+        img.src = 'modelo/noite.png'}
+        document.body.style.background = '#515154'
+        
     
 }
